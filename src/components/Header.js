99905cfb82e7934e76f1d8beb266263356646407
oscary garcia <elniodelrap2@hotmail.com>
2019-11-11 21:27:13 -0400

    add boostrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,34 @@
-import React from "react";
-import "../styles/components/Header.scss"
+import React from 'react';
+import '../styles/components/Header.scss';
 
 const Header = () => (
-  <div className="header">
-    <img
-      className="header__img"
-      src="../assets/logo-platzi-video-BW2.png"
-      alt="Platzi Video"
-    />
-    <div className="header__menu">
-      <div className="header__menu--profile">
-        <img src="../assets/user-icon.png" alt="" /><p>Perfil</p>
-      </div>
-      <ul>
-        <li>
-          <a href="/">Cuenta</a>
-        </li>
-        <li>
-          <a href="/">Cerrar Sesión</a>
-        </li>
-      </ul>
-    </div>
-  </div>
+  <ReactFragment>
+    <Row>
+      <Col>
+        <div className="header">
+          <img
+            className="header__img"
+            src="../assets/logo-platzi-video-BW2.png"
+            alt="Platzi Video"
+          />
+          <div className="header__menu">
+            <div className="header__menu--profile">
+              <img src="../assets/user-icon.png" alt="" />
+              <p>Perfil</p>
+            </div>
+            <ul>
+              <li>
+                <a href="/">Cuenta</a>
+              </li>
+              <li>
+                <a href="/">Cerrar Sesión</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </Col>
+    </Row>
+  </ReactFragment>
 );
 
 export default Header;
