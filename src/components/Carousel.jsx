04e8f13540cf0d30779b5873817f2f9d3fc898f4
oscary <oscarygs26@hotmail.com>
2019-12-09 18:12:22 -0400

    rename class to className

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import "../styles/components/Carousel.scss"
+import React from 'react';
 
-const Carousel = ({children}) => (
-  <div class="carousel">
-    <div class="carousel__container">
-        {children}
-    </div>
-  </div>
+import { Row } from 'reactstrap';
+import '../styles/components/Carousel.scss';
+
+const Carousel = ({ children }) => (
+  <Row className="carousel">
+    <div className="carousel__container">{children}</div>
+  </Row>
 );
 
 export default Carousel;
